b29a57da4d20d056096aa01af6c980fc68c7ae79
Matthew Wyskiel <developer@mattwyskiel.com>
2021-10-25 21:50:22 -0400

    Initial Commit

---
+++ BonesDay/BonesDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  BonesDayApp.swift
+//  BonesDay
+//
+//  Created by Matthew Wyskiel on 10/25/21.
+//
+
+import SwiftUI
+
+@main
+struct BonesDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BonesDayUITests/BonesDayUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BonesDayUITestsLaunchTests.swift
+//  BonesDayUITests
+//
+//  Created by Matthew Wyskiel on 10/25/21.
+//
+
+import XCTest
+
+class BonesDayUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
